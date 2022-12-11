d7d72631ffef902874b6d85403284eee696b7cad
khadija-Moussafi <kamassafi19@gmail.com>
2022-12-11 12:30:09 +0100

    first commit

---
+++ src/main/java/ma/ilisi/moveAndDescover/MoveAndDescoverApplication.java
@@ -0,0 +1,13 @@
+package ma.ilisi.moveAndDescover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveAndDescoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveAndDescoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ilisi/moveAndDescover/MoveAndDescoverApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ilisi.moveAndDescover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveAndDescoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
